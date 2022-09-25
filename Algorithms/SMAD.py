788af7eec57616fc268fce1cd893559a2b043e1c
Asghar Nazir <93176369+19f-0936@users.noreply.github.com>
2022-09-25 23:39:29 +0500

    Update SMAD.py

--- Algorithms/SMAD.py
+++ Algorithms/SMAD.py
@@ -1,30 +1,50 @@
 # Compute Total of the squares of all observations
-def SS(X,T):
-  result = list()
-  sum=0
-  for i, j in zip(X, T):
-    result.append(i * (j*j))
-  return result
+def SS(X, T):
+    result = list()
+    for i, j in zip(X, T):
+        result.append(i * (j * j))
+    res = sum(result)
+    return res
+
 
 # Compute Correction Factor
 def CF(T):
-  result = list()
-  s = len(T)
-  for i in zip(T):
-    result.append((i * i )/ s)
-  return result
+    result = list()
+    for i in T:
+        result.append((i * i) / len(T))
+    return sum(result)
+
 
 # Compute Total Sum of Squares
-def TotalSS(X,T):
-  ss=sum(X)
-  cf=sum(T)
-  result=ss-cf
-  return result
+def TotalSS(X, T):
+    ss = sum(SS(X, T))
+    cf = CF(T)
+    result = ss - cf
+    return result
 
-A=[1,2,3]
-B=[1,2,3]
-ss=SS(A,B)
 
-cf=CF(A)
-B=TotalSS(ss,A)
-print(B)
+# Compute Sum of Squares Total
+def SST(T, H):
+    cf = CF(T)
+    result = list()
+    for i in T:
+        result.append((i * i) / H - cf)
+    return sum(result)
+
+
+def SSB(T, X, K):
+    cf = CF(T)
+    result = list()
+    for i, j in zip(X, T):
+        result.append((i * (j * j)) / K - cf)
+    res = sum(result)
+    return res
+
+
+A = [1, 2, 3]
+B = [1, 2, 3]
+
+print(SSB(A, B, 1))
+ss = SS(A, B)
+
+cf = CF(A)
