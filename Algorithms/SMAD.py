7b35c2e598f2a8f39eb31fac4452003abc3eccaa
Asghar Nazir <93176369+19f-0936@users.noreply.github.com>
2022-09-25 23:12:04 +0500

    Creating totalSS functionality
    Creating totalSS functionality in SMAD Model

--- Algorithms/SMAD.py
+++ Algorithms/SMAD.py
@@ -1,6 +1,7 @@
 # Compute Total of the squares of all observations
 def SS(X,T):
   result = list()
+  sum=0
   for i, j in zip(X, T):
     result.append(i * (j*j))
   return result
@@ -8,7 +9,22 @@ def SS(X,T):
 # Compute Correction Factor
 def CF(T):
   result = list()
+  s = len(T)
   for i in zip(T):
-    result.append((i * i )/ len(T))
+    result.append((i * i )/ s)
   return result
 
+# Compute Total Sum of Squares
+def TotalSS(X,T):
+  ss=sum(X)
+  cf=sum(T)
+  result=ss-cf
+  return result
+
+A=[1,2,3]
+B=[1,2,3]
+ss=SS(A,B)
+
+cf=CF(A)
+B=TotalSS(ss,A)
+print(B)
