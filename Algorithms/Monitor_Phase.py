662121375e54b3595a3ca09779deaa49bb31a2cc
Asghar Nazir <93176369+19f-0936@users.noreply.github.com>
2022-10-01 00:58:50 +0500

    Update Monitor_Phase.py

--- Algorithms/Monitor_Phase.py
+++ Algorithms/Monitor_Phase.py
@@ -1,1 +1,13 @@
 # Algorithm for Monitor Phase
+import math
+
+
+def Dis(a, s):
+    result = list()
+    for i, j in zip(a, s):
+        result.append(math.sqrt((a[i] - s[j]) ** 2))
+    res = sum(result)
+    if res == 0:
+        print("Alert to machine")
+    else:
+        print("Accepting next feature ..")
