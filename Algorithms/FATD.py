164470d6208112572ab45299bc1daeb8cccf46c0
Asghar Nazir <93176369+19f-0936@users.noreply.github.com>
2022-09-30 23:41:37 +0500

    Update FATD.py
    Completed The FATD file

--- Algorithms/FATD.py
+++ Algorithms/FATD.py
@@ -1,1 +1,9 @@
 # Algorithm for File Anomaly Type Detection using Positive Selection Algorithm
+def Check(ab, Ag):
+    for i in Ag:
+        for j in ab:
+            if ab[j] == Ag[i]:
+                print("Self File")
+                return 0
+        print("Non Self File")
+        print("Reading File")
